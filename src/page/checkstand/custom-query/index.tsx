ebaf32a99fd86ba436e8f2a9c3103972e99c8853
caomengli2017 <15102114332@163.com>
2021-05-19 09:41:49 +0800

    add custom query

--- src/page/checkstand/custom-query/index.tsx
+++ src/page/checkstand/custom-query/index.tsx
@@ -3,14 +3,30 @@
  *@desc 自定义查询
  *@Date: 2021-05-17 18:18:16
  */
-import React from 'react';
-import { Flex, NavBar, WhiteSpace, WingBlank } from 'antd-mobile';
+import React, { useState } from 'react';
+import {
+  DatePicker,
+  Flex,
+  List,
+  NavBar,
+  WhiteSpace,
+  WingBlank,
+} from 'antd-mobile';
 import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
 import './index.less';
 const PREFIX = 'custom-query';
 const CustomQueryPage = () => {
   const history = useHistory();
+  const [queryMode, setQueryMode] = useState('');
+  const queryModeList = [
+    { mode: 'date', name: '昨日' },
+    { mode: 'week', name: '本周' },
+    { mode: 'month', name: '本月' },
+  ];
+  const switchMode = (mode: React.SetStateAction<string>) => {
+    setQueryMode(mode);
+  };
 
   return (
     <div style={{ width: '100%' }} className={`${PREFIX}`}>
@@ -22,6 +38,30 @@ const CustomQueryPage = () => {
         自定义查询
       </NavBar>
       <WhiteSpace />
+      <WingBlank>
+        <ul className="custom-time">
+          {queryModeList.map((item) => (
+            <li
+              onClick={() => switchMode(item.mode)}
+              key={item.mode}
+              className={item.mode === queryMode ? 'active' : ''}
+            >
+              {item.name}
+            </li>
+          ))}
+        </ul>
+        <p className="label">日期选择</p>
+        <List className="time-picker-list">
+          <DatePicker>
+            <List.Item>开始时间</List.Item>
+          </DatePicker>
+          <DatePicker>
+            <List.Item>结束时间</List.Item>
+          </DatePicker>
+        </List>
+      </WingBlank>
+      <WhiteSpace />
+      <WingBlank>aflafjkla</WingBlank>
     </div>
   );
 };
